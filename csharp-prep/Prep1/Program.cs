d4911ed257437bbcb36a748e61a2d990e15ab750
Aaron Heiner <aheiner2001@gmail.com>
2024-01-12 20:41:06 -0700

    my name is james bond, added code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+//          Scott
+// What is your last name? Burton
+
+// 
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+    
     }
 }
