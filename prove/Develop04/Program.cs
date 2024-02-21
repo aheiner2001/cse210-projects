c304d35b05c90bb43a28f32d71567561ba1aae9e
Aaron Heiner <aheiner2001@gmail.com>
2024-02-21 16:01:51 -0700

    added printMenu dunction

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
-    }
+        printMenu();
+        string option  = Console.ReadLine();
+
+        
+
+
+
+
+
+
+
+
+
+
+
+     void printMenu(){
+        Console.Clear();
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine(" 1. Start breathing activity");
+        Console.WriteLine(" 2. Start reflecting activity");
+        Console.WriteLine(" 3. Start listing activiy");
+        Console.WriteLine(" 4. Quit");
+        Console.Write(" Select a choice from the menu: ");
+    } 
 }
+    }
+
+
+
+
+   
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        int i = 0;
+        while ( i< 6){
+Console.Write("+");
+
+Thread.Sleep(500);
+
+Console.Write("\b \b"); // Erase the + character
+Console.Write("-"); // Replace it with the - character
+            i++;
+        }
+
     }
 }
