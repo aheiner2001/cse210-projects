9204c15d172b2b921a45df9a9724e85aa45c0e8e
Aaron Heiner <aheiner2001@gmail.com>
2024-03-11 17:49:39 -0600

    added the  separate classes

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        displayMenu();
+
+
+
+       
     }
+
+
+    
+   static void displayMenu(){
+        int points = 0;
+            Console.WriteLine($"\nYou have {points} points\n");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1. Create New Goals");
+            Console.WriteLine(" 2. List Goals");
+            Console.WriteLine(" 3. Save Goals");
+            Console.WriteLine(" 4. Load Goals");
+            Console.WriteLine(" 5. Record Event");
+            Console.WriteLine(" 6. Quit");
+            Console.Write("Select a choice from the menu:");
+
+        }
 }
