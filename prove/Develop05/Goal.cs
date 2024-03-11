d4dc7302eaec33cf784635f17babe63c500d15e0
Aaron Heiner <aheiner2001@gmail.com>
2024-03-11 17:54:13 -0600

    added few methods: basic structure

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,3 +1,28 @@
 class Goal{
-    
+    private string _name;
+    private string _description;
+    private int _points;
+    private bool _complete;
+
+
+
+
+    public void saveGoals(){
+
+    }
+    public void laodGoals(){
+
+    }
+    public void getName(){
+
+    }
+    public void getDescription(){
+
+    }
+
+    public void setPoints(){
+        
+    }
+
 }
+
